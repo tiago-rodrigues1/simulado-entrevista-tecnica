e90d8a31a56bf9fda3a9fbc31af3b408b054b18d
Tiago Rodrigues <tiagords127@gmail.com>
2022-08-17 10:09:58 -0300

    add q1

---
+++ Tabuada.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Tabuada {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        
+        for (int i = 1; i <= 10; i++) {
+            int result = N * i;
+            System.out.println(N + " x " + i + " = " + result);
+        }
+
+        bufferedReader.close();
+    }
+}
